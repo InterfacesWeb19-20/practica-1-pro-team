27c173f71f53e9349f825dc4bf6c6f0b2d535b75
Iván Pujante <pujante19863@iesmarenostrum.com>
2019-10-08 18:22:22 +0200

    Añado Section a vuestro contenido para arreglar el salto con el header y realizo cambios generales en mi css.
    Signed-off-by: Iván Pujante <pujante19863@iesmarenostrum.com>

--- js/header.js
+++ js/header.js
@@ -6,16 +6,12 @@ $(document).ready(function(){
 		if( $(this).scrollTop() > 0 ){
 
 			$('header').addClass('header2');
-			$('nav').addClass('.nav2');
-			$('header h1').addClass('.header2 h1');
-			//$('padding-top-salto').addClass('padding-top-salto');
+			$('section').addClass('espacioHeader');
 
 		} else {
 
 			$('header').removeClass('header2');
-			$('nav').removeClass('.nav2');
-			$('header h1').removeClass('.header2 h1');
-			//$('padding-top-salto').removeClass('padding-top-salto');
+			$('section').removeClass('espacioHeader');
 		}
 	});
 
